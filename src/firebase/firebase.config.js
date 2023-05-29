405eb7b6ec46753f7fbf200b2d1ef77e539f4784
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-29 11:41:45 +0600

    starting

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,3 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// ---------0----------------
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,12 +23,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyCVMGP8M2xEeyisiV2LwGgQcxBl3ywr0cs",
+  authDomain: "bistro-boss-client-dashboard.firebaseapp.com",
+  projectId: "bistro-boss-client-dashboard",
+  storageBucket: "bistro-boss-client-dashboard.appspot.com",
+  messagingSenderId: "159161455522",
+  appId: "1:159161455522:web:6e4c84374cf4ae61bd18fb"
 };
 
 // Initialize Firebase
